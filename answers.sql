bdde034d96d59f2c30ce936980f642e959fbb22f
endemic420 <201993355+endemic420@users.noreply.github.com>
2025-03-21 13:07:06 +0300

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,28 @@
+-- CREATE DATABASE salesDB;
+
+-- STEP 2 SELECT A DATABASE TO STORE TABLES 
+-- USE salesDB;
+
+-- STEP 3 CREATE TABLE CALLED PRODUCTS ADD COLUMNS 
+-- CREATE TABLE Products
+-- (
+-- Product_ID INT PRIMARY KEY AUTO_INCREMENT,
+-- Product_name VARCHAR (100),
+-- Price DECIMAL (10,2),
+-- Quantity INT 
+-- );
+
+-- STEP 4 MODIFY A TABLE 
+-- ALTER TABLE Products ADD COLUMN category VARCHAR (50);
+
+-- STEP 5 INSERT DATA ON THE TABLE 
+-- INSERT INTO Products( Product_name, Price, Quantity, Category)
+-- VALUES 
+-- ('Books', 100.50, 276, 'Stationery'),
+-- ('Pens', 30.00, 500, 'Stationery'),
+-- ('Pencils', 10.75, 120, 'Stationery'),
+-- ('Textbooks', 1000.99, 50, 'Stationery'),
+-- ('Rulers', 50.00, 60, 'Stationery'),
+-- ('Laptop', 20000.00, 10, 'Electronics'),
+-- ('Laptop bag', 500.50, 10, 'Fashion');
+
